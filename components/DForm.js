e9a796e4b2787e9e769882bfb93a27e74c3dc90a
Chris <richardsonchrisj@gmail.com>
2021-12-06 09:50:37 -0800

    deposits updated to persist

---
+++ components/DForm.js
@@ -0,0 +1,190 @@
+import { useState } from "react";
+import { useRouter } from "next/router";
+import { mutate } from "swr";
+
+const DForm = ({ formId, accountForm, forNewAccount = true }) => {
+  const router = useRouter();
+  const contentType = "application/json";
+  const [errors, setErrors] = useState({});
+  const [message, setMessage] = useState("");
+
+  const [form, setForm] = useState({
+    name: accountForm.name,
+    password: accountForm.password,
+    amount: accountForm.amount,
+    image_url: accountForm.image_url,
+  });
+
+  /* The PUT method edits an existing entry in the mongodb database. */
+  const putData = async (form) => {
+    const { id } = router.query;
+
+    try {
+      const res = await fetch(`/api/accounts/${id}`, {
+        method: "PUT",
+        headers: {
+          Accept: contentType,
+          "Content-Type": contentType,
+        },
+        body: JSON.stringify(form),
+      });
+
+      // Throw error with status code in case Fetch API req failed
+      if (!res.ok) {
+        throw new Error(res.status);
+      }
+
+      const { data } = await res.json();
+
+      mutate(`/api/accounts/${id}`, data, false); // Update the local data without a revalidation
+      alert(
+        `Contratulations, ${form.name} has been updated at Gotham City Bank!`
+      );
+
+      router.push("/secret");
+    } catch (error) {
+      setMessage("Failed to update account");
+    }
+  };
+
+  /* The POST method adds a new entry in the mongodb database. */
+  const postData = async (form) => {
+    try {
+      const res = await fetch("/api/accounts", {
+        method: "POST",
+        headers: {
+          Accept: contentType,
+          "Content-Type": contentType,
+        },
+        body: JSON.stringify(form),
+      });
+
+      // Throw error with status code in case Fetch API req failed
+      if (!res.ok) {
+        throw new Error(res.status);
+      }
+
+      alert(
+        `Contratulations, ${form.name} has opened an account at Gotham City Bank`
+      );
+      router.push("/secret");
+    } catch (error) {
+      setMessage("Failed to add account");
+    }
+  };
+
+  const handleChange = (e) => {
+    const target = e.target;
+    const value = target.value;
+    const name = target.name;
+
+    setForm({
+      ...form,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const errs = formValidate();
+    if (Object.keys(errs).length === 0) {
+      forNewAccount ? postData(form) : putData(form);
+    } else {
+      setErrors({ errs });
+    }
+  };
+
+  /* Makes sure pet info is filled for pet name, owner name, accountType, and image url*/
+  const formValidate = () => {
+    let err = {};
+    if (!form.name) err.name = "Name is required";
+    if (!form.password) err.amount = "A password is required";
+    if (!form.amount) err.amount = "starting amount is required";
+    if (!form.image_url) err.image_url = "Image URL is required";
+    return err;
+  };
+
+  const d5 = () => {
+    setForm({
+      ...form,
+      amount: (accountForm.amount += 5),
+    });
+  };
+  const d10 = () => {
+    setForm({
+      ...form,
+      amount: (accountForm.amount += 10),
+    });
+  };
+  const d20 = () => {
+    setForm({
+      ...form,
+      amount: (accountForm.amount += 20),
+    });
+  };
+  const d100 = () => {
+    setForm({
+      ...form,
+      amount: (accountForm.amount += 100),
+    });
+  };
+  const d1000 = () => {
+    setForm({
+      ...form,
+      amount: (accountForm.amount += 1000),
+    });
+  };
+  const d1m = () => {
+    setForm({
+      ...form,
+      amount: (accountForm.amount += 1000000),
+    });
+  };
+
+  const numbersWithCommas = (x) => {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  };
+
+  return (
+    <>
+      <p className="account-name">
+        New Balance:<p> ${numbersWithCommas(accountForm.amount)}</p>
+      </p>
+      <form id={formId} onSubmit={handleSubmit}>
+        <div className="text-center">
+          <button type="button" className="btn btn-dark" onClick={d5}>
+            $5
+          </button>
+          <button type="button" className="btn btn-dark" onClick={d10}>
+            $10
+          </button>
+          <button type="button" className="btn btn-dark" onClick={d20}>
+            $20
+          </button>
+          <button type="button" className="btn btn-dark" onClick={d100}>
+            $100
+          </button>
+          <button type="button" className="btn btn-dark" onClick={d1000}>
+            $1,000
+          </button>
+          <button type="button" className="btn btn-dark" onClick={d1m}>
+            $1M
+          </button>
+        </div>
+        <div class="d-grid gap-2">
+          <button type="submit" className="btn btn-danger">
+            Submit
+          </button>
+        </div>
+      </form>
+      <p>{message}</p>
+      <div>
+        {Object.keys(errors).map((err, index) => (
+          <li key={index}>{err}</li>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default DForm;
--- pages/[id]/deposit.js
+++ pages/[id]/deposit.js
@@ -1,95 +1,52 @@
-import { useState } from "react";
 import { useRouter } from "next/router";
-import Link from "next/link";
-import dbConnect from "../../lib/dbConnect";
-import Account from "../../models/Account";
+import useSWR from "swr";
+import DForm from "../../components/DForm";
 
-const DepositPage = ({ account }) => {
+const fetcher = (url) =>
+  fetch(url)
+    .then((res) => res.json())
+    .then((json) => json.data);
+
+const EditAccount = () => {
   const router = useRouter();
-  const [message, setMessage] = useState("");
-  const handleDelete = async () => {
-    const accountID = router.query.id;
+  const { id } = router.query;
+  const { data: account, error } = useSWR(
+    id ? `/api/accounts/${id}` : null,
+    fetcher
+  );
 
-    try {
-      await fetch(`/api/accounts/${accountID}`, {
-        method: "Delete",
-      });
-      router.push("/");
-    } catch (error) {
-      setMessage("Failed to delete the account.");
-    }
+  if (error) return <p>Failed to load</p>;
+  if (!account) return <p>Loading...</p>;
+
+  const accountForm = {
+    name: account.name,
+    password: account.password,
+    amount: account.amount,
+    image_url: account.image_url,
   };
 
   const numbersWithCommas = (x) => {
     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
   };
 
-  const [balance, setBalance] = useState(account.amount);
-
-  const depositFive = () => {
-    setBalance(balance + 5);
-  };
-  const depositTen = () => {
-    setBalance(balance + 10);
-  };
-  const depositTwenty = () => {
-    setBalance(balance + 20);
-  };
-  const depositHundred = () => {
-    setBalance(balance + 100);
-  };
-  const depositThousand = () => {
-    setBalance(balance + 1000);
-  };
-  const depositMillion = () => {
-    setBalance(balance + 1000000);
-  };
   return (
-    <div key={account._id}>
-      <div className="card">
-        <img src={account.image_url} />
-        <h5 className="account-name">{account.name}</h5>
-        <div className="main-content">
-          <p className="account-name">{account.name}</p>
-          <p className="account-name">${numbersWithCommas(balance)}</p>
-          {message && <p>{message}</p>}
+    <div className="card">
+      <img src={account.image_url} />
+      <h5 className="account-name">{account.name}</h5>
+      <div className="main-content">
+        <p className="account-name">{account.name}</p>
+        <p className="account-name">
+          Current Balance: ${numbersWithCommas(account.amount)}
+        </p>
 
-          <div className="btn-container text-center">
-            <button className="btn edit" onClick={depositFive}>
-              $5
-            </button>
-
-            <button className="btn edit" onClick={depositTen}>
-              $10
-            </button>
-
-            <button className="btn edit" onClick={depositTwenty}>
-              $20
-            </button>
-
-            <button className="btn edit" onClick={depositHundred}>
-              $100
-            </button>
-            <button className="btn edit" onClick={depositThousand}>
-              $1,000
-            </button>
-            <button className="btn edit" onClick={depositMillion}>
-              $1 Million
-            </button>
-          </div>
-        </div>
+        <DForm
+          formId="edit-account-form"
+          accountForm={accountForm}
+          forNewAccount={false}
+        />
       </div>
     </div>
   );
 };
 
-export async function getServerSideProps({ params }) {
-  await dbConnect();
-
-  const account = await Account.findById(params.id).lean();
-  account._id = account._id.toString();
-
-  return { props: { account } };
-}
-
-export default DepositPage;
+export default EditAccount;
